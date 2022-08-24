3a75e236322f8596906716ae9c081374c7e730f1
LitigatedParadox <away38099@gmail.com>
2022-08-24 18:17:32 -0500

    Added OS path and started work on detecting keys

---
+++ HotKeyMapper.py
@@ -0,0 +1,19 @@
+import subprocess
+from pynput.keyboard import *
+import os
+HotKey = {Key.ctrl_l, Key.alt, Key.shift}
+
+def press_on(key):
+    print('Press ON: {0}'.format(key))
+
+def press_off(key):
+    print('Press OFF: {0}'.format(key))
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=press_on, on_release=press_off) as listener:
+    listener.join()
+
+
+if press_on(Key.shift):
+    subprocess.call([r'C:\Users\Shahed\CMDSTart.bat']) 
