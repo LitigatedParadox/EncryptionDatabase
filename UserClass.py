d038f5d137ee45b77e19100a74ddf288ee7c3859
LitigatedParadox <away38099@gmail.com>
2023-01-29 12:07:58 -0600

    Added comments :Chad:

--- UserClass.py
+++ UserClass.py
@@ -2,35 +2,40 @@ import cryptography
 from cryptography import fernet as f
 from cryptography.fernet import Fernet
 import json
-import base64
-textfile = open('new.txt', 'a')
+#Initialize empty dictionary
 dict = {}
 
+'''#Instantiate class
 class Recipients:
 
     def __init__(self, name, key):
         self.name = name
-        self.key = key
+        self.key = key'''
 
+#Function to add a recipient to the database 
 def AddUser():
     Repeat = True
     while Repeat == True:
         with open('Pairs.json', 'r') as Pairs:
+        #Checks file to see whether or not it is empty. If it is empty, writes a Dummy element to the dictionary that is later deleted
             LineOne = Pairs.readline()
         if LineOne != "{\n":
             with open("Pairs.json", "w") as Ti83Plus:
                 Ti83Plus.write('{"Dummy": "Null"}')
+        #Takes input from user for new recipient's name, generates cryptographic key to be paired to user given recipient
         with open('Pairs.json', 'r') as Pairs:
             dict = json.load(Pairs)
             name = input("Enter the name by which this user will be called: ")
             key = str(Fernet.generate_key())
             dict[name] = key
+        #Deletes Dummy value from dictionary if it is found, then writes recipient key pairs to dictionary. If Dummy value is not in dictionary, simply writes given values to dictionary
         with open('Pairs.json', 'w') as storage:
             try:
                 del dict["Dummy"]
                 json.dump(dict, storage, indent = 4)
             except:
                 json.dump(dict, storage, indent= 4)
+        #Asks if user would like to add another user to the database. If yes: passes to the top of the loop. If no: breaks out of loop
         Terminate = input("Would you like to add another user to the database? (Y/N): ")
         if Terminate == 'N':
             with open('Pairs.json', 'w') as storage:
@@ -40,33 +45,42 @@ def AddUser():
         elif Terminate == "Y":
             pass
 
+#Function to remove user from the database
 def RemoveUser():
+    #Takes input from user to determine which user will be removed from the database
     with open('Pairs.json', 'r') as Pears:
         dict = json.load(Pears)
         print(dict.keys())
         name = input("Please enter the name of the user you would like to remove from the database? ")
         del dict[name]
         #print("The waffle house has found its new host:", name)
+    #Writes new dictionary with deleted values to json file
     with open('Pairs.json', 'w') as Pairs:
         json.dump(dict, Pairs, indent = 4)
-        
+
+#Function to list users in databse
 def ListUsers():
     with open("Pairs.json", "r") as storage:
         storage = json.load(storage)
-        print(storage.keys())
+        print(str(storage.keys()))
 
 def WriteMessage():
     with open("Pairs.json", 'r') as Pairs:
         Pairs = json.load(Pairs)
-        print(Pairs.keys())
+        print(str(Pairs.keys()))
+        #Takes input from user pertaining to recipient
         Recipient = input("To whom would you like to send your message? ")
         key = str(Pairs[Recipient])
+        #Strips b string of b and '
         key = key.strip("b")
         key = key.strip("'")
+        #Encodes string into bytes to be used for Fernet key
         Finalkey = bytes(key, 'utf-8')
         EncryptionKey = Fernet(Finalkey)
+        #Receives message from user to be encrypted 
         Message = input("Please enter the message you would like to encrypt here: ")
         EncryptedMessage = EncryptionKey.encrypt(bytes(Message, 'utf-8'))
+        #Converts EncryptedMessage to string to and strips message of b and ' before printing
         EncryptedMessage = str(EncryptedMessage)
         EncryptedMessage = EncryptedMessage.strip('b')
         EncryptedMessage = EncryptedMessage.strip("'")
@@ -95,7 +109,7 @@ def DecryptMessage():
     with open("Pairs.json", "r") as storage:
         storage = json.load(storage)
         EncryptedMessage = input("Please enter the encrypted message you receieved here: ")
-        print(storage.keys())
+        print(str(storage.keys()))
         Sender = input("From whom did you receive this message? ")
         SenderKey = str(storage[Sender])
         SenderKey = SenderKey.strip("b")
